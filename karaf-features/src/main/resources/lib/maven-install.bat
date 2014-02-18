2538044770beffee2cb88f3ddd3bd22a1ed05605
minikiller <minikiller@qq.com>
2014-02-18 16:19:02 +0800

    IRIS-4 修改karaf-feature，增加lib目录，添加注册amdatu类库

---
+++ karaf-features/src/main/resources/lib/maven-install.bat
@@ -0,0 +1,18 @@
+@ echo off
+::auto install amdatu jar
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.mongo -Dversion=1.0.4 -Dpackaging=jar -Dfile=org.amdatu.mongo-1.0.4.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.scheduling.api -Dversion=1.0.0 -Dpackaging=jar -Dfile=org.amdatu.scheduling.api-1.0.0.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.scheduling.quartz -Dversion=1.0.1 -Dpackaging=jar -Dfile=org.amdatu.scheduling.quartz-1.0.1.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.search.api -Dversion=1.0.0 -Dpackaging=jar -Dfile=org.amdatu.search.api-1.0.0.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.search.solr.impl -Dversion=1.0.1 -Dpackaging=jar -Dfile=org.amdatu.search.solr.impl-1.0.1.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.security.tokenprovider.api -Dversion=1.0.0 -Dpackaging=jar -Dfile=org.amdatu.security.tokenprovider.api-1.0.0.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.security.tokenprovider.impl -Dversion=1.0.0 -Dpackaging=jar -Dfile=org.amdatu.security.tokenprovider.impl-1.0.0.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.security.tokenstorage.mongo -Dversion=1.0.1 -Dpackaging=jar -Dfile=org.amdatu.security.tokenstorage.mongo-1.0.1.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.web.resourcehandler -Dversion=1.0.4 -Dpackaging=jar -Dfile=org.amdatu.web.resourcehandler-1.0.4.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.web.rest.doc -Dversion=1.1.1 -Dpackaging=jar -Dfile=org.amdatu.web.rest.doc-1.1.1.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.web.rest.jaxrs -Dversion=1.0.0 -Dpackaging=jar -Dfile=org.amdatu.web.rest.jaxrs-1.0.0.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.amdatu -DartifactId=org.amdatu.web.rest.wink -Dversion=1.0.8 -Dpackaging=jar -Dfile=org.amdatu.web.rest.wink-1.0.8.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=net.vz.mongodb.jackson -DartifactId=mongo-jackson-mapper -Dversion=1.4.3 -Dpackaging=jar -Dfile=net.vz.mongodb.jackson.mongo-jackson-mapper-1.4.3.jar -DgeneratePom=true
+
+
+
